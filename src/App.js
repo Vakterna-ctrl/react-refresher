63dc99a296b4fe38e5b7252f48aa0c63ff59ffda
chaukhadoan <kha.doanchau@gmail.com>
2022-11-25 23:42:32 +0100

    fjsodfj

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       name: "Kha",
+      company: "Bolag",
     };
   }
 
@@ -16,7 +17,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
+          <p>
+            Hi {this.state.name} i work at {this.state.company}
+          </p>
           <button
             onClick={() => {
               this.setState({ name: "Khaleesi" });
